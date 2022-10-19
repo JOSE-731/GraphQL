133e637f7e0a4a4d18c22640dc70a0bfe11f4d32
Jose David <jose7312000@gmail.com>
2022-10-19 17:54:07 -0500

    Custom type

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const { graphql, buildSchema } = require('graphql')//Importamos la utilidades para trabajar con los schemas 
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql') //Importamos el middleware de express graphql
+const {readFileSync} = require('fs') //Utilizamos el modulo fs para leer el archivo
+const {join} = require('path')//Utilizamos join para unir el path del archivo
+const resolvers = require('./lib/resolvers')
 const app = express()
 
 const port = 3000
 
 //Definimos el esquema
-const schema = buildSchema(`
- type Query{
-    hello: String,
-    name: String
- }
-`)
-
-//Configuracion de resolvers: definimos lo que devolverá la consulta
-const resolvers = {
-    hello: () => {
-        return 'Hola mundo'
-    },
-    name: () => {
-        return 'Jose'
-    }
-}
+const schema = buildSchema(readFileSync(
+    join(
+        __dirname, 'lib', 'schema.graphql'
+    ), 'utf-8'
+))
 
 //Ejecutamos el query
 /*graphql(schema, '{hello, name}', resolvers).then((data) => {
@@ -29,7 +21,6 @@ const resolvers = {
 })*/
 
 //Definimos la ruta
-
 app.use('/api', graphqlHTTP({
     schema: schema, //Pasamos el schema que definimos arriba
     rootValue: resolvers, //Pasamos el resolver
